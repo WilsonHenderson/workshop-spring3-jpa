69292d5bb8b0ab053540f9a9c82dc7ecdce4f809
Wilson Henderson <wilsonenderson01@gmail.com>
2025-10-23 15:12:33 -0300

    OrderStatus enum

--- src/main/java/com/projetoweb/course/entities/Order.java
+++ src/main/java/com/projetoweb/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetoweb.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,71 +16,91 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb-order")
-public class Order  implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; 
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone ="GMT")
-	private  Instant moment;
-	
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-	
-	public Order() {
-	}
+@Table(name = "tb_order")
+public class Order implements Serializable {
 
-	public Order(Long id, Instant moment, User client) {
-		super();
-		this.id = id;
-		this.moment = moment;
-		this.client = client;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public Long getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
+        timezone = "GMT"
+    )
+    private Instant moment;
 
-	public Instant getMoment() {
-		return moment;
-	}
+    private Integer orderStatus;
 
-	public void setMoment(Instant moment) {
-		this.moment = moment;
-	}
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
 
-	public User getClient() {
-		return client;
-	}
+    // Construtor padrão
+    public Order() {
+    }
 
-	public void setClient(User client) {
-		this.client = client;
-	}
+    // Construtor com parâmetros
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        super();
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf( orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+    	if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+    	}   
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    // Métodos equals e hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        return Objects.equals(id, other.id);
+    }
 }
